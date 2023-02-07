b3a416016ba60ca8ab33de5b7c489460332d38b5
Dikshita Kalita <kthedikshita@gmail.com>
2023-02-07 11:40:33 +0100

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/env bash
+
+exec >log.out 2>&1   #Display all teh results of teh scripts below in a log.out file
+
+ 
+Diskspace=$(df --total)    #Display disk space
+echo -e "The disk space in system is: $Diskspace"
+
+#ps -ef             #List processes
+
+Uptime=$(uptime)    #Display uptime and load average of system
+echo "The system uptime and its load average is: $Uptime"
+
+
+TCP=$(ss -s)               #Listing active TCP connections to system 
+echo "Active TCP connections to the system are: $TCP"
+
+Temp=$(sensors)             #Listing the temperature of the system
+echo "Temperature of the system is: $Temp"
+
+
+IP_address=$(ip -4 -o a | cut -d '' -f 2,7 | cut -d '/' -f 1)      #Showing IP address of system including network interfaces and IPV4 on the system
+echo "IP address and network interface of system: $IP_address"
+
+
+Hostname=$(hostname --ip-addresses)         #Displaying hostname of system
+echo "The hostname is: $Hostname"
+
+
+
